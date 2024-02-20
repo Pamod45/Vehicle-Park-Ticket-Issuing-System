62c62adaa45916fd78335643cd64853c6664e0e8
Pamod45 <pubupere32@gmail.com>
2024-02-20 18:12:20 +0530

    Merge Developer2Nisal Doubly Linked List

---
+++ src/carparkmanagementsystem/dataStructures/DayList.java
@@ -0,0 +1,34 @@
+package carparkmanagementsystem.dataStructures;
+import java.util.Date;
+
+public class DayList 
+{
+    DailyCustomerList head;
+    DailyCustomerList tail;
+
+
+    public DailyCustomerList addDay(String date) {
+        DailyCustomerList newDay = new DailyCustomerList(date);
+        if (head == null) {
+            head = tail = newDay;
+        } else {
+            tail.next = newDay;
+            newDay.previous = tail;
+            tail = newDay;
+        }
+        return newDay;
+        
+    }
+    
+    public DailyCustomerList getCustomerList(String date){
+        DailyCustomerList current = head;
+        while(current!=null){
+            if(current.date==date){
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+}
